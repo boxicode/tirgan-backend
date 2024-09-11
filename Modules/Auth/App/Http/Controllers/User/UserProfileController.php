50c649bd3d2382a0a3155c779c3df33747bd7469
yamata <codeindevelopment@gmail.com>
2024-09-12 02:55:06 +0330

    implement user auth base controller

---
+++ Modules/Auth/App/Http/Controllers/User/UserProfileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Auth\App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserProfileController extends Controller
+{
+    //
+}
